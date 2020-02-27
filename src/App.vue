5e1da4c46207e38df2de369f01fa0ae810f8d2c9
gacent <794733453@qq.com>
2020-02-27 16:58:13 +0800

    feat：回归默认模板

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,8 @@
 <template>
   <div>
-    <MyTree :data="data"></MyTree>
+    vue-collection
   </div>
 </template>
 
 <script>
-import MyTree from '@/components/MyTree.vue'
-import { getTreeList } from './api'
-export default {
-  components: {
-    MyTree
-  },
-  data () {
-    return {
-      data: []
-    }
-  },
-  mounted () {
-    getTreeList().then((res) => {
-      const data = res.data
-      //  1. 扁平数据变成多层数据 递归数据
-      this.data = [...data.parent, ...data.child] // 数据所有合并
-    })
-  }
-}
 </script>
-
-<style lang="scss" scoped>
-
-</style>
